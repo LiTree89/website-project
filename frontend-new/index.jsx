a583d5f83ef4b8c7580768dd4670e824759a8b5c
dyingbreed243-8051 <dyinybreed243@gmail.com>
2025-12-28 03:01:12 -0500

    commit
    Signed-off-by: dyingbreed243-8051 <dyinybreed243@gmail.com>

--- frontend-new/index.jsx
+++ frontend-new/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return <h1>LitBit Frontend is working!</h1>;
-}
+import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
