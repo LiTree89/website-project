466f73e42d44448e2d43996b12c5e1550f0c74f2
dyingbreed243-8051 <dyinybreed243@gmail.com>
2025-12-28 17:12:24 -0500

    style: standardize string quotes in packs.js for consistency
    Signed-off-by: dyingbreed243-8051 <dyinybreed243@gmail.com>

--- api/packs.js
+++ api/packs.js
@@ -1,11 +1,11 @@
 // api/packs.js
-const { buyPack } = require('../backend/userDb.js');
+const { buyPack } = require("../backend/userDb.js");
 
 module.exports = async function (context, req) {
   const { userId, packType } = req.body;
   try {
     const user = await buyPack(userId, packType);
-    context.res = { status: 200, body: { message: 'Pack purchased', user } };
+    context.res = { status: 200, body: { message: "Pack purchased", user } };
   } catch (error) {
     context.res = { status: 400, body: { error: error.message } };
   }
