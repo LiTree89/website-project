c053e98dd142dda8b7c5f1ad946e3847ee8daaff
dyingbreed243-8051 <dyinybreed243@gmail.com>
2025-12-28 02:46:17 -0500

    commit
    Signed-off-by: dyingbreed243-8051 <dyinybreed243@gmail.com>

---
+++ frontend-new/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>LitBit Frontend is working!</h1>
+      {/* Add your navigation and components here */}
+    </div>
+  );
+}
